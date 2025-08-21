e881e3638e447b2e00f8d571cfaf022ba97983b1
David Santiago García Hernandez <santidav0613@gmail.com>
2025-08-20 19:11:56 -0500

    Subiendo archivo

---
+++ TareaIntegradoraAPO1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TareaIntegradoraAPO1{
+    public static void main (String []args){
+        Scanner scann =new Scanner(System.in);
+    System.out.print("Ingrese una palabra porfavor ");
+        String n= scann.nextLine();
+    System.out.println(n);
+    scann.close();
+    }
+}
